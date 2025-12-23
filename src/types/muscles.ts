3273a7a375859e11f92205df9e65c6838561a3bf
Joseph Scafidi <jscafidi616@hotmail.com>
2025-12-23 00:28:22 -0600

    Update for AbsObliques sides

--- src/types/muscles.ts
+++ src/types/muscles.ts
@@ -1,5 +1,5 @@
 // Auto-generated from SVG
-export type MuscleId = 'Body_Bg' | 'Body_Stroke' | 'RightForearm' | 'RightBicep' | 'RightShoulder' | 'LeftForearm' | 'LeftBicep' | 'LeftShoulder' | 'RightCalf' | 'RightQuad' | 'RightAdductor' | 'LeftCalf' | 'LeftQuad' | 'LeftAdductor' | 'Chest' | 'AbsObliques' | 'AbsUpper' | 'AbsLower' | 'Neck' | 'RightKnee' | 'LeftKnee' | 'RightFeet' | 'LeftFeet' | 'LeftHand' | 'RightHand';
+export type MuscleId = 'Body_Bg' | 'Body_Stroke' | 'RightForearm' | 'RightBicep' | 'RightShoulder' | 'LeftForearm' | 'LeftBicep' | 'LeftShoulder' | 'RightCalf' | 'RightQuad' | 'RightAdductor' | 'LeftCalf' | 'LeftQuad' | 'LeftAdductor' | 'Chest' | 'RightAbsObliques' | 'LeftAbsObliques' | 'AbsUpper' | 'AbsLower' | 'Neck' | 'RightKnee' | 'LeftKnee' | 'RightFeet' | 'LeftFeet' | 'LeftHand' | 'RightHand';
 
 export type MuscleGroupName = 'Body' | 'RightArm' | 'LeftArm' | 'RightLeg' | 'LeftLeg' | 'Torso' | 'Abs' | 'No-Muscle' | 'Knees' | 'Feets' | 'Hands';
 
